7424cefd0f588b6e4308adba6c2d7f54fa77dc92
Sartaj <sartaj@sartaj.me>
2023-02-24 08:28:06 +0000

    update typescript

--- api.ts
+++ api.ts
@@ -1,13 +1,5 @@
-import axios from "axios";
-import { CiviLegislationData } from "./types";
-
 export const civiLegislationApi = {
-  getLegislationData: async (
-    locale: "chicago" | "illinois" | "usa"
-  ): Promise<CiviLegislationData[]> => {
-    const result = await axios.get<CiviLegislationData[]>(
-      `https://github.com/civi-social/civi-legislation-data/releases/download/nightly/${locale}.legislation.json`
-    );
-    return result.data;
+  getLegislationDataUrl: (locale: "chicago" | "illinois" | "usa"): string => {
+    return `https://github.com/civi-social/civi-legislation-data/releases/download/nightly/${locale}.legislation.json`;
   },
 };
