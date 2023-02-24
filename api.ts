ec2ae72dfcd5ea044117be9b828b935e7b128335
Sartaj <sartaj@sartaj.me>
2023-02-24 07:57:10 +0000

    add types to return

--- api.ts
+++ api.ts
@@ -1,9 +1,13 @@
 import axios from "axios";
+import { CiviLegislationData } from "./types";
 
 export const civiLegislationApi = {
-  getLegislationData: async (locale: "chicago" | "illinois" | "usa") => {
-    return axios.get(
+  getLegislationData: async (
+    locale: "chicago" | "illinois" | "usa"
+  ): Promise<CiviLegislationData> => {
+    const result = await axios.get<CiviLegislationData>(
       `https://github.com/civi-social/civi-legislation-data/releases/download/nightly/${locale}.legislation.json`
     );
+    return result.data;
   },
 };
