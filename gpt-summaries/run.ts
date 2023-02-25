9254644a393f346151884ad919c837d66cc06b0f
Sartaj <sartaj@sartaj.me>
2023-02-25 06:04:50 +0000

    update

--- gpt-summaries/run.ts
+++ gpt-summaries/run.ts
@@ -1,8 +1,9 @@
-import axios from "axios";
+import axios, { AxiosError, AxiosHeaders, AxiosRequestConfig } from "axios";
 import fs, { writeFile } from "fs";
 import path from "path";
 import { CiviLegislationData, locales, Locales } from "../api";
 import { writeJSON } from "../scraper/writeFile";
+import axiosRetry from "axios-retry";
 
 if (!process.env.OPEN_API_KEY) {
   console.error("Need to provide OPEN_API_KEY as environment var");
@@ -10,8 +11,15 @@ if (!process.env.OPEN_API_KEY) {
 }
 const OPEN_API_KEY = process.env.OPEN_API_KEY;
 
-async function summarizeText(apiKey: string, text: string): Promise<string> {
-  const response = await axios.post(
+type OpenAiReturn = {
+  choices: { text: string }[];
+};
+
+async function summarizeText(
+  apiKey: string,
+  text: string
+): Promise<OpenAiReturn> {
+  const summary = await postWithRetry<OpenAiReturn>(
     "https://api.openai.com/v1/completions",
     {
       model: "text-davinci-003",
@@ -28,9 +36,31 @@ async function summarizeText(apiKey: string, text: string): Promise<string> {
     }
   );
 
-  const summary = response.data;
-
   return summary;
+}
+
+async function postWithRetry<T extends object>(
+  url: string,
+  body: object,
+  config: AxiosRequestConfig,
+  retries = 3
+): Promise<T> {
+  try {
+    const response = await axios.post<T>(url, body, config);
+    return response.data;
+  } catch (e: unknown) {
+    const error = e as AxiosError<T>;
+    if (error.response && error.response.status === 429 && retries > 0) {
+      const waitTime = Math.pow(2, 4 - retries) * 1000; // Exponential backoff with max wait time of 8 seconds
+      console.log(
+        `Too Many Requests. Retrying in ${waitTime / 1000} seconds...`
+      );
+      await new Promise((resolve) => setTimeout(resolve, waitTime));
+      return await postWithRetry(url, body, config, retries - 1);
+    } else {
+      throw error;
+    }
+  }
 }
 
 const legislationAddSummaries = async (locale: Locales) => {
@@ -39,30 +69,24 @@ const legislationAddSummaries = async (locale: Locales) => {
     "utf8"
   );
   const legislations = JSON.parse(jsonStr) as CiviLegislationData[];
-  const legislationWithAi = [...legislations];
-  for (let legislation of legislations) {
-    const text = legislation.title + "\n" + legislation.description;
-    // console.log("====ORIGINAL====\n\n");
-    // console.log(text.trim());
-    // console.log("\n\n");
-    // console.log("====GPT====\n");
+  const legislationWithAi = [] as CiviLegislationData[];
 
-    return summarizeText(OPEN_API_KEY, text.trim())
-      .then((s: any) => {
-        legislationWithAi[i2].summaries = {
-          gpt: s.choices[0].text.trim(),
-        };
-      })
-      .catch((e) => {
-        console.error("error");
-        console.error(e);
-      });
-  });
+  for (const legislation of legislations) {
+    const text = legislation.title + "\n" + legislation.description;
+
+    const s = await summarizeText(OPEN_API_KEY, text.trim());
+
+    legislationWithAi.push({
+      ...legislation,
+      summaries: {
+        gpt: s.choices[0].text.trim(),
+      },
+    });
+  }
 
   writeJSON(`${locale}.legislation`, legislationWithAi);
-
-  }
 };
+
 locales.forEach((locale) => {
   legislationAddSummaries(locale);
 });
