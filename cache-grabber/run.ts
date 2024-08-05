8e47238ec30476c03bcc75ce834e38a6c5d81c27
Sartaj <sartaj@sartaj.me>
2024-08-04 22:41:09 -0500

    update

--- cache-grabber/run.ts
+++ cache-grabber/run.ts
@@ -1,12 +1,12 @@
-// script to get current release data for any reason
+// Download Current release data from the API and write it to the filesystem
 
-import { civiLegislationApi } from "../api";
+import { forEachLocale } from "../api/utils";
 import { writeGptJSON, writeLegislationJSON } from "../fs/write-file";
 import { getCachedGpt, getCachedLegislation } from "./get";
 
 export const retrieveCurrentRelease = async () => {
   try {
-    civiLegislationApi.locales.forEach(async (locale) => {
+    forEachLocale(async (locale) => {
       const legislation = await getCachedLegislation(locale);
       const gpt = await getCachedGpt(locale);
       writeLegislationJSON(locale, legislation);
